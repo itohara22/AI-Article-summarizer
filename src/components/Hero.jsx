aa8886cd6b173535426bf5c372b8a5441fc73368
itohara22 <ito.k23018@gmail.com>
2023-04-28 19:29:54 +0530

    link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/itohara22.com", "_blank")
+            window.open("https://github.com/itohara22", "_blank")
           }
           className='black_btn'
         >
